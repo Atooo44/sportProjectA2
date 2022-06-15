aa7752fec20ac7aff6e97e643af73c4a78c30853
BastienHuard <bastien.huard@isen-ouest.yncrea.fr>
2022-06-15 09:28:33 +0200

    create sport.php + add default.css

--- php/index.php
+++ php/index.php
@@ -10,11 +10,12 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="../css/default.css" rel="stylesheet">
         <link href="../css/homepage.css" rel="stylesheet">
         <title> Sportmate</title>
     </head>
     <body>
-        <section class="green">
+        <section>
             <header>
                 <a href="index.php"><img src="../ressources/logo_match.svg" class="logo"></a>
                 <div class="toggle"></div>
